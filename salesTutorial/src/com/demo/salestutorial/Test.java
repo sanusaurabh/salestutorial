f546f885c5360e3da14684484e5b3c9e27afc52b
sanusaurabh <31151180+sanusaurabh@users.noreply.github.com>
2017-08-19 15:34:19 +0530

    Update Test.java

--- salesTutorial/src/com/demo/salestutorial/Test.java
+++ salesTutorial/src/com/demo/salestutorial/Test.java
@@ -2,7 +2,7 @@ package com.demo.salestutorial;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello   sales world");
+		System.out.println("Hello   sales world test ");
 	}
 
 }
