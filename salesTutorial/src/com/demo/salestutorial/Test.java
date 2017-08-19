5c6854b3229d3fccaec7cc6065252117f2961c40
sanusaurabh <san.619jaan@gmail.com>
2017-08-19 15:46:25 +0530

    test 2 commiit

--- salesTutorial/src/com/demo/salestutorial/Test.java
+++ salesTutorial/src/com/demo/salestutorial/Test.java
@@ -2,7 +2,7 @@ package com.demo.salestutorial;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello   sales world test ");
+		System.out.println("Hello   sales world test  new ");
 	}
 
 }
